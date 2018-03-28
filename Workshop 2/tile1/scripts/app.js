0d7e38f8377a189a2724e0d655fbe2484158dab6
Jnewton3 <35235293+Jnewton3@users.noreply.github.com>
2018-03-28 18:44:22 -0400

    Add files via upload

---
+++ Workshop 2/tile1/scripts/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){     
+	// col-1 blue and pink stripes
+for (i=1;i<101;i++){
+	$('.column-1').append('<div class="stripe"></div>')
+}
+
+for (i=1;i<400;i++){
+	$('.column-2').append('<div class="stripe"></div>')
+}
+
+
+});
---
+++ Workshop 2/tile2/scripts/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){     
+	
+for (i=1;i<600;i++){
+	$('.column-1').append('<div class="stripe"></div>')
+}
+
+for (i=1;i<200;i++){
+	$('.column-2').append('<div class="stripe"></div>')
+}
+
+
+});
